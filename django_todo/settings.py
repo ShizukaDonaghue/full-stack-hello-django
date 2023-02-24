4ea44118fb4f16302c4facabf137defbb194b26d
Shizuka Donaghue <sdonaghue@gmail.com>
2023-02-24 12:34:42 +0000

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_ejvgfjf9pf(=7m8mzsc%aso=#i7#jo^n#4gul-fevxdt2^@je
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shizuka-todo-app.herokuapp.com']
 
 
 # Application definition
